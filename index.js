c60b61d86803075ab14d32992fce08483322e785
Yosef <shmuli5770@gmail.com>
2021-03-13 20:18:39 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 function addingEventListener() {
-}
+    const input = document.getElementById('input');
+    const div5 = document.querySelector('div div div div div');
+
+    input.addEventListener('click', clickAlert);
+    div5.addEventListener('click', clickAlert);
+
+    function clickAlert() {
+        alert('Hee hee, that tickles!');
+    }
+  }
